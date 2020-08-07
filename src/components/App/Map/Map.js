bac7f3ecf44ad207be8537c564f51ce24a29f424
toutpt <toutpt@gmail.com>
2020-08-07 12:26:38 +0200

    doc: publish

--- src/components/App/Map/Map.js
+++ src/components/App/Map/Map.js
@@ -3,35 +3,43 @@ import { useState, useEffect } from "preact/hooks";
 import './Map.css';
 import { lambert93ToWGS84 } from "../../../coordinate";
 
+
+const PLUS_LAT = 0.03;
+const PLUS_LON = 0.02;
+
 export function Map(props) {
     const [mapref, setMapRef] = useState({});
     const [xy, setxy] = useState([]);
     useEffect(() => {
         const entries = props.entries || [];
         if (mapref.map) {
+            mapref.geoJSON.forEach(g => g.remove());
+            mapref.geoJSON = [];
             entries.forEach(entry => {
                 const config = parse(entry.content);
                 const coord = lambert93ToWGS84(config.xllcorner, config.yllcorner);
-                debugger;
-                if (mapref.geoJSON) {
-                    mapref.geoJSON.remove();
-                }
                 mapref.map.setView(coord, 10);
                 const data = {
                     "type": "FeatureCollection",
                     "features": [
                         {
                             "type": "Feature",
+                            "properties": {},
                             "geometry": {
                                 "type": "Polygon",
-                                "coordinates": [ coord, [coord[0], coord[1] + 0.5], [coord[0] + 0.5, coord[1] + 0.5], [coord[0] +0.5, coord[1]] ],
+                                "coordinates": [[
+                                    [coord[1], coord[0]],
+                                    [coord[1], coord[0] + PLUS_LON],
+                                    [coord[1] + PLUS_LAT, coord[0] + PLUS_LON],
+                                    [coord[1] + PLUS_LAT, coord[0]],
+                                    [coord[1], coord[0]]
+                                ]],
                             },
                         }
                     ],
                 };
-                console.log(JSON.stringify(data, null, 2));
-                mapref.geoJSON = L.geoJSON(data).addTo(mapref.map);
-                
+                console.log(JSON.stringify(data));
+                mapref.geoJSON.push(L.geoJSON(data).addTo(mapref.map));
             });
         } else {
             
@@ -39,12 +47,10 @@ export function Map(props) {
             const layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(mymap);
-            const geoJSON = L.geoJSON().addTo(mymap);
-            console.log({geoJSON})
             setMapRef({
                 map: mymap,
                 layer,
-                geoJSON,
+                geoJSON: [],
             });
         }
     }, [props.entries]);
